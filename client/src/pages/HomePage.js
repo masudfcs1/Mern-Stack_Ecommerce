e73118e6216cebc87883fce607ec2168a619f0d7
masudfcs1 <masudfcs1@gmail.com>
2023-07-08 20:34:29 +0600

    Dune

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
-
+import "../styles/Homepage.css";
 const HomePage = () => {
   const [cart, setCart] = useCart();
   const [products, setProducts] = useState([]);
